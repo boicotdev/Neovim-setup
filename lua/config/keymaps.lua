0b59ed6f25002215b874fc47fb6f96a90d88c549
boicotdev <carlos.guzman93@live.com>
2026-01-25 18:59:11 -0500

    I reorganized the folders tructure to make more readeble

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,21 @@
+-- =============================================================================
+-- KEYMAPS
+-- =============================================================================
+
+local keymap = vim.keymap.set
+local opts = { noremap = true, silent = true }
+
+-- -------------------------------------------------
+-- Buffers
+-- -------------------------------------------------
+keymap("n", "<leader>bn", ":bnext<CR>", opts)
+keymap("n", "<leader>bp", ":bprevious<CR>", opts)
+keymap("n", "<leader>bd", ":bdelete<CR>", opts)
+
+-- -------------------------------------------------
+-- Window navigation
+-- -------------------------------------------------
+keymap("n", "<leader>h", "<C-w>h", opts)
+keymap("n", "<leader>j", "<C-w>j", opts)
+keymap("n", "<leader>k", "<C-w>k", opts)
+keymap("n", "<leader>l", "<C-w>l", opts)
